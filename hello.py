5b7e403e77e49e66a0564a6c4df7ee02cd8a8c23
Andre Blasque <andre.blasque@gmail.com>
2023-10-03 10:40:12 -0300

    Adicionado gitignore

--- hello.py
+++ hello.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
+"""
+Hello multi linguas.
+
+Dependendo da lingua consigurada no
+ambiente, o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+     export LANG=pt_BR
+
+Execucao:
+
+     python3 hello.py
+     ou
+     ./hello.py
+
+"""
+
+__version__ = "0.0.1"
+__author__ = "Andre Blasque" 
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
 
 
-# Este soft eh o primeiro em python3
-print("Hello !!!")
-print("teste do git")
+
+print(msg)
